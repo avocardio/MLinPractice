c5a28d7f9276f9b0cadbc3cdafce7c9d43b2e625
avocardio <mkalcher@uni-osnabrueck.de>
2021-10-29 10:54:16 -0600

    last changes

--- code/classification.sh
+++ code/classification.sh
@@ -3,13 +3,13 @@
 # create directory if not yet existing
 mkdir -p data/classification/
 
-classifiers=("SGDClassifier LinearSVC LogisticRegression")
+classifiers=("LogisticRegression")
 for k in $classifiers
 do
     echo $k
     # run feature extraction on training set (may need to fit extractors)
     echo "  training set"
-    python -m code.classification.run_classifier data/feature_extraction/training.pickle -e data/classification/classifier.pickle --accuracy --kappa --balanced_accuracy --classification_report --balanced_data_set --$k --verbose
+    python -m code.classification.run_classifier data/feature_extraction/training.pickle -e data/classification/classifier.pickle --accuracy --kappa --balanced_accuracy --classification_report --balanced_data_set --$k #--verbose
     # run feature extraction on validation set (with pre-fit extractors)
     echo "  validation set"
     python -m code.classification.run_classifier data/feature_extraction/validation.pickle -i data/classification/classifier.pickle --accuracy --kappa --balanced_accuracy --classification_report # --small 10000
--- code/pipeline.sh
+++ code/pipeline.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # overall pipeline for the ML experiments
 
-echo -e "/nloading data"
+echo -e "\n loading data"
 code/load_data.sh
-echo -e "/npreprocessing"
+echo -e "\n preprocessing"
 code/preprocessing.sh
-echo -e "/nfeature extraction"
+echo -e "\n feature extraction"
 code/feature_extraction.sh
 #echo "dimensionality reduction"
 #code/dimensionality_reduction.sh
-echo -e "/nclassification"
+echo -e "\n classification"
 code/classification.sh
